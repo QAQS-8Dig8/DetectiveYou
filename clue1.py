35231bdd15ad93ed9919ec6b56724e58bf95f877
QAQS-8Dig8 <63666083+QAQS-8Dig8@users.noreply.github.com>
2020-04-21 11:19:09 -0500

    Update clue1.py

--- clue1.py
+++ clue1.py
@@ -4,10 +4,13 @@ from pygame.locals import *
 import win32gui, win32con 
 import time 
 
-pygame.mixer.pre_init(44100,16,2,4096)
+#mostly changed the places where you call textObject and put in parameters. 
+#Also changed a few places where you has FONT in all caps. At least in my version it needed "Font"
+#I moved a few lines of code at the beginning here tha I think need to go inside the clueOne function definition
+
+pygame.mixer.pre_init(44100,16,2,4096) #you don't need this unless you want to play background music
 pygame.init()
-win32gui.ShowWindow(hwnd, win32con.SW_MAXIMIZE)
-pygame.display.set_caption('Clue 1')
+
 
 def textObject(text, font, text_color):
     textSurface = font.render(text, True, text_color)
@@ -15,59 +18,73 @@ def textObject(text, font, text_color):
 
 def clueOne(): 
 
-	screen = pygame.display.set_mode((500, 500), HWSURFACE | DOUBLEBUF | RESIZABLE)
-	
-	#backgroundimage
-	background = pygame.image.load('olmstedcartoon.JPG')
-	pygame.display.set_icon(background)
-	screen.fill((0,0,0))
-	screen.blit(pygame.transform.scale(background, (500,500)), (0,0))
-	pygame.display.flip()
+    screen = pygame.display.set_mode((500, 500), HWSURFACE | DOUBLEBUF | RESIZABLE)
+    
+#this stuff needs to be inside the function
+    hwnd = win32gui.GetForegroundWindow() #you need this for the next line
+    win32gui.ShowWindow(hwnd, win32con.SW_MAXIMIZE)
+    pygame.display.set_caption('Clue 1')
+    
+#backgroundimage
+    background = pygame.image.load('olmstedcartoon.JPG')
+    pygame.display.set_icon(background)
+    screen.fill((0,0,0))
+    screen.blit(pygame.transform.scale(background, (500,500)), (0,0))
+    pygame.display.flip()
 
-	#riddle
-	riddle = pygame.draw.rect(screen, [255,255,255], [500,275, 530, 340])
-	riddle_text = pygame.font.Font("freesansbold.ttf", 20)
-	textSurf, textRect = textObject("Riddle: Although I’m not magic, I am a type of bean. I’m something you can drink that has lots of caffeine. Please select the answer the riddle, if you are correct you will be transported to the next clue if not, you will get to choose again.", riddle_text, [125,125,125])
-	screen.blit(textSurf,textRect)
-	pygame.display.update() 
-
-
-	#coffeebutton 
-	coffee_button_color = [215,132,121]
-	coffee_button = pygame.draw.circle(screen, coffee_cup_color, (100,215), 500)
-	coffee_button_text = pygame.font.FONT("freesansbold.ttf", 20)
-	textSurf, textPolygon = textObject()
-	pygame.display.update()
+    #riddle
+    riddle = pygame.draw.rect(screen, [255,255,255], [500,275, 530, 340])
+    riddle_text = pygame.font.Font("freesansbold.ttf", 20)
+    textSurf, textRect = textObject("Riddle: Although I’m not magic, I am a type of bean. I’m something you can drink that has lots of caffeine. Please select the answer the riddle, if you are correct you will be transported to the next clue if not, you will get to choose again.", riddle_text, [125,125,125])
+    screen.blit(textSurf,textRect)
+    pygame.display.update() 
 
 
-	pygame.event.pump()
+    #coffeebutton 
+    coffee_button_color = [215,132,121]
+    coffee_cup_color = [0,0,0]
+    coffee_button = pygame.draw.circle(screen, coffee_cup_color, (100,215), 500)
+    coffee_button_text = pygame.font.Font("freesansbold.ttf", 20)
+    
+    #i added parameters but i don't know what you actually want for text or text color
+    textSurf, textPolygon = textObject("coffee", coffee_button_text, [0,0,0]) 
+    pygame.display.update()
 
-	for event in pygame.event.get():
+#you need a while loop here but I think you might need to put all the button stuff from above into it too
+
+    pygame.event.pump()
+
+    for event in pygame.event.get():
             pos = pygame.mouse.get_pos()
             if event.type == pygame.quit: 
-                pygame.mixer.music.stop()
+                pygame.mixer.music.stop() #I don't think you have any music file pulled in
                 quite()
                 
             if 100 > pos[0] > 215 and  100 > pos[1] > 215:
                 coffee_button_color = [215,132,121]
+                
+                #I'm a little confused on the various button variables. why do we need a begin button?
                 begin_button = pygame.draw.cirlce(screen, coffee_button_color, (100,215), 500)
                 button_text = pygame.font.Font("freesansbold.ttf", 20)
+                    #i added parameters but i don't know what you actually want for text or text color
                 textSurf, textCircle = textObject("Coffee", button_text, [0,0,0])
                 screen.blit(textSurf, textCircle)
                 pygame.display.update()
                 if event.type == pygame.get_pressed(1,0,0):
                     coffee_button_color = [55, 115, 150]
                     coffee_begin_button = pygame.draw.ellipse(screen, button_color, (100,215), 500)
-                    coffee_button_text = pygame.font.Font("freesansbold.ttf", 20)
-                    textSurf, textCircle = textObject("Coffee", button_text, [0,0,0])
+                    coffee_button_text = pygame.font.Font("freesansbold.ttf", 20) #@could just use button_text
+                        #i added parameters but i don't know what you actually want for text or text color
+                    textSurf, textCircle = textObject("Coffee", coffee_button_text, [0,0,0])
                     screen.blit(textSurf, textCircle)
                     pygame.display.update()
                     clueTwo(screen, button)
             else:
                 coffee_button_color = [215, 132, 121]
-                coffee_button = pygame.draw.circle(screen, button_color, (100,215), 500)
+                coffee_button = pygame.draw.circle(screen, coffee_button_color, (100,215), 500)
                 coffee_button_text = pygame.font.Font("freesansbold.ttf", 20)
-                textSurf, textCircle = textObject("Begin", button_text, [0,0,0])
+                    #i added parameters but i don't know what you actually want for text or text color
+                textSurf, textCircle = textObject("Begin", coffee_button_text, [0,0,0])
                 screen.blit(textSurf, textCircle)
                 pygame.display.update()
            
@@ -79,15 +96,16 @@ def clueOne():
 
     #sodabutton
     soda_button_color = [215,132,121]
-	soda_button = pygame.draw.circle(screen, coffee_cup_color, (100,215), 500)
-	soda_button_text = pygame.font.FONT("freesansbold.ttf", 20)
-	textSurf, textCircle = textObject()
-	pygame.display.update()
+    soda_button = pygame.draw.circle(screen, coffee_cup_color, (100,215), 500)
+    soda_button_text = pygame.font.Font("freesansbold.ttf", 20)
+    textSurf, textCircle = textObject("soada", soda_button_text, [0,0,0])
+    pygame.display.update()
 
 
-	pygame.event.pump()
+    pygame.event.pump()
 
-	for event in pygame.event.get():
+#I don't understand the need for multiple event for loops but maybe it will make sense once I see the code working
+    for event in pygame.event.get():
             pos = pygame.mouse.get_pos()
             if event.type == pygame.quit: 
                 pygame.mixer.music.stop()
@@ -114,7 +132,7 @@ def clueOne():
                 begin_button = pygame.draw.circle(screen, button_color, (700,800), 500)
                 button_text = pygame.font.Font("freesansbold.ttf", 20)
                 textSurf, textEllipse = textObject("Begin", button_text, [0,0,0])
-                textEllipse.center = (100, 215
+                textEllipse.center = (100, 215)
                 screen.blit(textSurf, textCircle)
                 pygame.display.update()
            
@@ -126,15 +144,15 @@ def clueOne():
 
     #redbullbutton
     redbull_button_color = [215,132,121]
-	redbull_button = pygame.draw.circle(screen, coffee_cup_color, (1400, 1500), 500)
-	redbull_button_text = pygame.font.FONT("freesansbold.ttf", 20)
-	textSurf, textCircle = textObject()
-	pygame.display.update()
+    redbull_button = pygame.draw.circle(screen, coffee_cup_color, (1400, 1500), 500)
+    redbull_button_text = pygame.font.Font("freesansbold.ttf", 20)
+    textSurf, textCircle = textObject("redbull", redbull_button_text, [0,0,0])
+    pygame.display.update()
 
 
-	pygame.event.pump()
+    pygame.event.pump()
 
-	for event in pygame.event.get():
+    for event in pygame.event.get():
             pos = pygame.mouse.get_pos()
             if event.type == pygame.quit: 
                 pygame.mixer.music.stop()
