4952e592a88205208fa279db5174a879addaf604
QAQS-8Dig8 <63666083+QAQS-8Dig8@users.noreply.github.com>
2020-04-26 22:29:47 -0500

    Update clue1.py

--- clue1.py
+++ clue1.py
@@ -12,14 +12,28 @@ def textObject(text, font, text_color):
     textSurface = font.render(text, True, text_color)
     return textSurface, textSurface.get_rect()
 
+def wrong_answer():
+    message = tk.Tk()
+    tk.Label(message, text="Wrong answer!").grid(row=0)
+    tk.Label(message, text="Be careful! You only get 3 wrong answers per game!").grid(row=1)
+    
+    message.mainloop()
 
-def clueOne(): 
+def game_over():
+    you_lost = tk.Tk()
+    tk.Label(you_lost, text="Game Over!").grid(row=1)
+    you_lost.mainloop()
+    pygame.display.quit()
+    welcomescreen()
 
+def clueOne(e1, e2, e3): 
+    pygame.display.quit()
 
-   # name = e1.get()
-   # major = e2.get()
-   # guide = e3.get()
-  #  pygame.mixer.music.stop()
+    name = e1.get()
+    major = e2.get()
+    guide = e3.get()
+    pygame.mixer.music.stop()
+    wrong_answers = 0
 
     screen = pygame.display.set_mode((500, 500), HWSURFACE | DOUBLEBUF | RESIZABLE)
     
@@ -37,9 +51,9 @@ def clueOne():
     
 
     
-    coffee_button_color = [215,132,121]
-    soda_button_color = [215,132,121]
-    redbull_button_color = [215,132,121]
+    option1_button_color = [215,132,121]
+    option2_button_color = [215,132,121]
+    option3_button_color = [215,132,121]
     loop = True 
 
     while(loop): 
@@ -50,27 +64,27 @@ def clueOne():
         screen.blit(textSurf, textRect)
         pygame.display.update()
         
-        coffee_button = pygame.draw.ellipse(screen, coffee_button_color, [700, 600, 250, 100])
-        coffee_button_text = pygame.font.Font("freesansbold.ttf",35)
-        textSurf, textEllipse = textObject("Richer flavor", coffee_button_text, [0,0,0])
+        option1_button = pygame.draw.ellipse(screen, option1_button_color, [700, 600, 250, 100])
+        option1_button_text = pygame.font.Font("freesansbold.ttf",35)
+        textSurf, textEllipse = textObject("Richer flavor", option1_button_text, [0,0,0])
         textEllipse.center = ((700+(250/2)), (600+(100/2)))
         screen.blit(textSurf, textEllipse)
         pygame.display.update()
     
         
         
-        soda_button = pygame.draw.ellipse(screen, soda_button_color, [1000, 600, 250, 100])
-        soda_button_text = pygame.font.Font("freesansbold.ttf",35)
-        textSurf, textEllipse = textObject("Less caffeine", soda_button_text, [0,0,0])
+        option2_button = pygame.draw.ellipse(screen, option2_button_color, [1000, 600, 250, 100])
+        option2_button_text = pygame.font.Font("freesansbold.ttf",35)
+        textSurf, textEllipse = textObject("Less caffeine", option2_button_text, [0,0,0])
         textEllipse.center = ((1000+(250/2)), (600+(100/2)))
         screen.blit(textSurf, textEllipse)
         pygame.display.update()
     
         
         
-        redbull_button = pygame.draw.ellipse(screen, redbull_button_color, [400, 600, 250, 100])
-        redbull_button_text = pygame.font.Font("freesansbold.ttf",35)
-        textSurf, textEllipse = textObject("Both", redbull_button_text, [0,0,0])
+        option3_button = pygame.draw.ellipse(screen, option3_button_color, [400, 600, 250, 100])
+        option3_button_text = pygame.font.Font("freesansbold.ttf",35)
+        textSurf, textEllipse = textObject("Both", option3_button_text, [0,0,0])
         textEllipse.center = ((400+(250/2)), (600+(100/2)))
         screen.blit(textSurf, textEllipse)
         pygame.display.update()
@@ -80,104 +94,115 @@ def clueOne():
         for event in pygame.event.get():
             pos = pygame.mouse.get_pos()
             if event.type == pygame.quit: 
-                quite()
+                pygame.display.quite()
                 
             if 700+250 > pos[0] > 700 and 600+100 > pos[1] > 600:
-                coffee_button_color = [55, 115, 250]
-                coffee_button = pygame.draw.ellipse(screen, coffee_button_color, [700, 600, 250, 100])
-                coffee_button_text = pygame.font.Font("freesansbold.ttf",35)
-                textSurf, textEllipse = textObject("Richer flavor", coffee_button_text, [0,0,0])
+                option1_button_color = [55, 115, 250]
+                option1_button = pygame.draw.ellipse(screen, option1_button_color, [700, 600, 250, 100])
+                option1_button_text = pygame.font.Font("freesansbold.ttf",35)
+                textSurf, textEllipse = textObject("Richer flavor", option1_button_text, [0,0,0])
                 textEllipse.center = ((700+(250/2)), (600+(100/2)))
                 screen.blit(textSurf, textEllipse)
                 pygame.display.update()
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    coffee_button_color = [55, 115, 150]
-                    coffee_button = pygame.draw.ellipse(screen, coffee_button_color, [700, 600, 250, 100])
-                    coffee_button_text = pygame.font.Font("freesansbold.ttf",35)
-                    textSurf, textEllipse = textObject("Richer flavor", coffee_button_text, [0,0,0])
+                    option1_button_color = [55, 115, 150]
+                    option1_button = pygame.draw.ellipse(screen, option1_button_color, [700, 600, 250, 100])
+                    option1_button_text = pygame.font.Font("freesansbold.ttf",35)
+                    textSurf, textEllipse = textObject("Richer flavor", option1_button_text, [0,0,0])
                     textEllipse.center = ((700+(250/2)), (600+(100/2)))
                     screen.blit(textSurf, textEllipse)
                     pygame.display.update()
                 if event.type == pygame.MOUSEBUTTONUP:
-                    coffee_button_color = [215, 132, 121]
-                    coffee_button = pygame.draw.ellipse(screen, coffee_button_color, [700, 600, 250, 100])
-                    coffee_button_text = pygame.font.Font("freesansbold.ttf",35)
-                    textSurf, textEllipse = textObject("Richer flavor", coffee_button_text, [0,0,0])
+                    option1_button_color = [215, 132, 121]
+                    option1_button = pygame.draw.ellipse(screen, option1_button_color, [700, 600, 250, 100])
+                    option1_button_text = pygame.font.Font("freesansbold.ttf",35)
+                    textSurf, textEllipse = textObject("Richer flavor", option1_button_text, [0,0,0])
                     textEllipse.center = ((700+(250/2)), (600+(100/2)))
                     screen.blit(textSurf, textEllipse)
                     pygame.display.update()
+                    wrong_answers += 1
+                    if wrong_answers > 3:
+                        game_over()
+                    else:
+                        wrong_answer()
             else:
-                coffee_button_color = [215, 132, 121]
-                coffee_button = pygame.draw.ellipse(screen, coffee_button_color, [700, 600, 250, 100])
-                coffee_button_text = pygame.font.Font("freesansbold.ttf",35)
-                textSurf, textEllipse = textObject("Richer flavor", coffee_button_text, [0,0,0])
+                option1_button_color = [215, 132, 121]
+                option1_button = pygame.draw.ellipse(screen, option1_button_color, [700, 600, 250, 100])
+                option1_button_text = pygame.font.Font("freesansbold.ttf",35)
+                textSurf, textEllipse = textObject("Richer flavor", option1_button_text, [0,0,0])
                 textEllipse.center = ((700+(250/2)), (600+(100/2)))
                 screen.blit(textSurf, textEllipse)
                 pygame.display.update()
                 
             if 1000+250 > pos[0] > 1000 and 600+100 > pos[1] > 600:
-                soda_button_color = [55, 115, 250]
-                soda_button = pygame.draw.ellipse(screen, soda_button_color, [1000, 600, 250, 100])
-                soda_button_text = pygame.font.Font("freesansbold.ttf",35)
-                textSurf, textEllipse = textObject("Less caffeine", soda_button_text, [0,0,0])
+                option2_button_color = [55, 115, 250]
+                option2_button = pygame.draw.ellipse(screen, option2_button_color, [1000, 600, 250, 100])
+                option2_button_text = pygame.font.Font("freesansbold.ttf",35)
+                textSurf, textEllipse = textObject("Less caffeine", option2_button_text, [0,0,0])
                 textEllipse.center = ((1000+(250/2)), (600+(100/2)))
                 screen.blit(textSurf, textEllipse)
                 pygame.display.update()
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    soda_button_color = [55, 115, 150]
-                    soda_button = pygame.draw.ellipse(screen, soda_button_color, [1000, 600, 250, 100])
-                    soda_button_text = pygame.font.Font("freesansbold.ttf",35)
-                    textSurf, textEllipse = textObject("Less caffeine", soda_button_text, [0,0,0])
+                    option2_button_color = [55, 115, 150]
+                    option2_button = pygame.draw.ellipse(screen, option2_button_color, [1000, 600, 250, 100])
+                    option2_button_text = pygame.font.Font("freesansbold.ttf",35)
+                    textSurf, textEllipse = textObject("Less caffeine", option2_button_text, [0,0,0])
                     textEllipse.center = ((1000+(250/2)), (600+(100/2)))
                     screen.blit(textSurf, textEllipse)
                     pygame.display.update()
                 if event.type == pygame.MOUSEBUTTONUP:
-                    soda_button_color = [215, 132, 121]
-                    soda_button = pygame.draw.ellipse(screen, soda_button_color, [1000, 600, 250, 100])
-                    soda_button_text = pygame.font.Font("freesansbold.ttf",35)
-                    textSurf, textEllipse = textObject("Less caffeine", soda_button_text, [0,0,0])
+                    option2_button_color = [215, 132, 121]
+                    option2_button = pygame.draw.ellipse(screen, option2_button_color, [1000, 600, 250, 100])
+                    option2_button_text = pygame.font.Font("freesansbold.ttf",35)
+                    textSurf, textEllipse = textObject("Less caffeine", option2_button_text, [0,0,0])
                     textEllipse.center = ((1000+(250/2)), (600+(100/2)))
                     screen.blit(textSurf, textEllipse)
                     pygame.display.update()
+                    wrong_answers += 1
+                    if wrong_answers > 3:
+                        game_over()
+                    else:
+                        wrong_answer()
             else:
-                soda_button_color = [215, 132, 121]
-                soda_button = pygame.draw.ellipse(screen, soda_button_color, [1000, 600, 250, 100])
-                soda_button_text = pygame.font.Font("freesansbold.ttf",35)
-                textSurf, textEllipse = textObject("Less caffeine", soda_button_text, [0,0,0])
+                option2_button_color = [215, 132, 121]
+                option2_button = pygame.draw.ellipse(screen, option2_button_color, [1000, 600, 250, 100])
+                option2_button_text = pygame.font.Font("freesansbold.ttf",35)
+                textSurf, textEllipse = textObject("Less caffeine", option2_button_text, [0,0,0])
                 textEllipse.center = ((1000+(250/2)), (600+(100/2)))
                 screen.blit(textSurf, textEllipse)
                 pygame.display.update()
             
             if 400+250 > pos[0] > 400 and 600+100 > pos[1] > 600:
-                redbull_button_color = [55, 115, 250]
-                redbull_button = pygame.draw.ellipse(screen, redbull_button_color, [400, 600, 250, 100])
-                redbull_button_text = pygame.font.Font("freesansbold.ttf",35)
-                textSurf, textEllipse = textObject("Both", redbull_button_text, [0,0,0])
+                option3_button_color = [55, 115, 250]
+                option3_button = pygame.draw.ellipse(screen, option3_button_color, [400, 600, 250, 100])
+                option3_button_text = pygame.font.Font("freesansbold.ttf",35)
+                textSurf, textEllipse = textObject("Both", option3_button_text, [0,0,0])
                 textEllipse.center = ((400+(250/2)), (600+(100/2)))
                 screen.blit(textSurf, textEllipse)
                 pygame.display.update()
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    redbull_button_color = [55, 115, 150]
-                    redbull_button = pygame.draw.ellipse(screen, redbull_button_color, [400, 600, 250, 100])
-                    redbull_button_text = pygame.font.Font("freesansbold.ttf",35)
-                    textSurf, textEllipse = textObject("Both", redbull_button_text, [0,0,0])
+                    option3_button_color = [55, 115, 150]
+                    option3_button = pygame.draw.ellipse(screen, option3_button_color, [400, 600, 250, 100])
+                    option3_button_text = pygame.font.Font("freesansbold.ttf",35)
+                    textSurf, textEllipse = textObject("Both", option3_button_text, [0,0,0])
                     textEllipse.center = ((400+(250/2)), (600+(100/2)))
                     screen.blit(textSurf, textEllipse)
                     pygame.display.update()
                 if event.type == pygame.MOUSEBUTTONUP:
-                    redbull_button_color = [215, 132, 121]
-                    redbull_button = pygame.draw.ellipse(screen, redbull_button_color, [400, 600, 250, 100])
-                    redbull_button_text = pygame.font.Font("freesansbold.ttf",35)
-                    textSurf, textEllipse = textObject("Both", redbull_button_text, [0,0,0])
+                    option3_button_color = [215, 132, 121]
+                    option3_button = pygame.draw.ellipse(screen, option3_button_color, [400, 600, 250, 100])
+                    option3_button_text = pygame.font.Font("freesansbold.ttf",35)
+                    textSurf, textEllipse = textObject("Both", option3_button_text, [0,0,0])
                     textEllipse.center = ((400+(250/2)), (600+(100/2)))
                     screen.blit(textSurf, textEllipse)
                     pygame.display.update()
+                    #clueTwo(e1, e2, e3, wrong_answers)
                     
             else:
-                redbull_button_color = [215, 132, 121]
-                redbull_button = pygame.draw.ellipse(screen, redbull_button_color, [400, 600, 250, 100])
-                redbull_button_text = pygame.font.Font("freesansbold.ttf",35)
-                textSurf, textEllipse = textObject("Both", redbull_button_text, [0,0,0])
+                option3_button_color = [215, 132, 121]
+                option3_button = pygame.draw.ellipse(screen, option3_button_color, [400, 600, 250, 100])
+                option3_button_text = pygame.font.Font("freesansbold.ttf",35)
+                textSurf, textEllipse = textObject("Both", option3_button_text, [0,0,0])
                 textEllipse.center = ((400+(250/2)), (600+(100/2)))
                 screen.blit(textSurf, textEllipse)
                 pygame.display.update()
@@ -186,7 +211,3 @@ def clueOne():
                 screen = pygame.display.set_mode(event.dict['size'], HWSURFACE | DOUBLEBUF | RESIZABLE)
                 screen.blit(pygame.transform.scale(background, event.dict['size']), (0, 0))
                 pygame.display.flip()
-
-
- 
-clueOne()
